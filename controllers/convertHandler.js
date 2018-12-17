f937f7e9aea89b9b03fd5516353be6184b5608d7
Peter Newton <peter@4sightdesigns.com>
2018-12-17 15:16:56 +0000

    initUnit logic

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -5,19 +5,61 @@
 *       
 *       
 */
+const validateUnit = (val) => {
+
+  const units = {
+    gal: "gal",
+    gals: "gal",
+    gallons: "gal",
+    l: "L",
+    L: "L",
+    liters: "L",
+    lb: "lbs",
+    lbs: "lbs",
+    pounds: "lbs",
+    kg: "kg",
+    Kg: "kg",
+    Kgs: "kg",
+    kilograms: "kg",
+    mi: "mi",
+    miles: "mi",
+    km: "km",
+    Km: "km",
+    kilometers: "km"
+  }
+
+  let test = units[val]
+
+  if (!test) {
+    return "invalid unit"
+  }
+  return test
+}
 
 function ConvertHandler() {
+
   
   this.getNum = function(input) {
-    var result;
-    
-    return result;
+
+    const splitPoint = input.search(/[^\.|^\d|^\/]/)
+    let value = input.slice(0, splitPoint)
+    if (!value) {
+      value = 1
+    } else {
+      if (value.match(/\/+/g) && value.match(/\/+/g).length > 1) {
+        value = "invalid number"
+      }
+    }
+
+    return value
   };
   
   this.getUnit = function(input) {
-    var result;
     
-    return result;
+    const splitPoint = input.search(/[^\.|^\d|^\/]/)
+    let value = input.slice(splitPoint)
+    let validated = validateUnit(value)
+    return validated
   };
   
   this.getReturnUnit = function(initUnit) {
