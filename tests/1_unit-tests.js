0faf6f96f882b13eed4247e4aa2bbef766fd09a2
Peter Newton <peter@4sightdesigns.com>
2018-12-18 11:28:02 +0000

    fix typo

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -101,42 +101,42 @@ suite('Unit Tests', function(){
   
   suite('Function convertHandler.convert(num, unit)', function() {
     
-    test('Gal to L', function(done) {
+    test('gal to L', function(done) {
       var input = [5, 'gal'];
       var expected = 18.92705;
       assert.approximately(convertHandler.convert(input[0],input[1]),expected,0.1); //0.1 tolerance
       done();
     });
     
-    test('L to Gal', function(done) {
+    test('L to gal', function(done) {
       const input = [2, 'L'];
       const expected = 0.52834;
       assert.approximately(convertHandler.convert(input[0], input[1]), expected, 0.1)
       done();
     });
     
-    test('Mi to Km', function(done) {
-      const input = [2, 'M'];
+    test('mi to Km', function(done) {
+      const input = [2, 'mi'];
       const expected = 3.21868;
       assert.approximately(convertHandler.convert(input[0], input[1]), expected, 0.1)
       done();
     });
     
-    test('Km to Mi', function(done) {
+    test('km to mi', function(done) {
       const input = [2, 'km'];
       const expected = 1.24275;
       assert.approximately(convertHandler.convert(input[0], input[1]), expected, 0.1)
       done();
     });
     
-    test('Lbs to Kg', function(done) {
+    test('lbs to kg', function(done) {
       const input = [2, 'lbs'];
       const expected = 0.90718;
       assert.approximately(convertHandler.convert(input[0], input[1]), expected, 0.1)
       done();
     });
     
-    test('Kg to Lbs', function(done) {
+    test('kg to lbs', function(done) {
       const input = [2, 'kg'];
       const expected = 4.40925;
       assert.approximately(convertHandler.convert(input[0], input[1]), expected, 0.1)
